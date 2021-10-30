6f77aadba36f317bab9faa1f2fe5ebd1fd441894
krista-t <krista.tomicic@gmail.com>
2021-10-30 17:36:43 +0200

    title

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,8 +1,16 @@
+import fairdata from "../assets/fairdata.png";
 const Footer = () => {
 	const today = new Date();
 
 	return (
 		<footer className="footer">
+			<a
+				href="https://fairdatacollective.com"
+				target="_blank"
+				rel="noreferrer">
+				<img height={90} src={fairdata} alt="logo" />
+			</a>
+
 			<p>
 				Designed and Developed by FAIR Data Collective
 				&copy; {today.getFullYear()}
